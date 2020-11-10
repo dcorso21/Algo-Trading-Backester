4f4e0f188ab26bbe3de25dd9adc3782952e06500
dcorso21 <david.l.corson@gmail.com>
2020-11-10 13:02:32 -0500

    sidebar complete

--- documentation_assets/index.js
+++ documentation_assets/index.js
@@ -6,18 +6,33 @@ toc = toc.querySelectorAll("ul")[0];
 
 enableExpansion(toc.children);
 
-function enableExpansion(children) {
-    console.log(children);
+function enableExpansion(children, layer = 1) {
+    // console.log(children);
     [...children].map((n) => {
-        let uls = n.querySelectorAll("ul"),
+        let uls = n.querySelectorAll(`.lay${layer+1}`),
             expand = n.querySelector(".expand");
 
         if (!expand) return;
 
         expand.onclick = () => {
+            if (layer === 1) {
+                let lay2 = selectAll(".lay2"),
+                    lay3 = selectAll(".lay3");
+                [...lay2, ...lay3].map((el) => {
+                    if (
+                        !el.classList.contains("toggleview") &&
+                        ![...uls].includes(el)
+                    ) {
+                        el.classList.add("toggleView");
+                    }
+                });
+            }
+
+            console.log(uls);
+
             [...uls].map((el) => {
                 el.classList.toggle("toggleView");
-                enableExpansion(el.children);
+                enableExpansion(el.children, layer+1);
             });
         };
     });
